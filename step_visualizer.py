7c9b7c3b4fcf34427c85120596bdc342948475d5
Armando Diaz Tolentino <ajdt@pikachu>
2014-05-09 19:01:36 -0700

    added some code to visualize the steps asp uses to solve an equation

---
+++ step_visualizer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Author:	Armando Diaz Tolentino <ajdt@cs.washington.edu> 
+#
+# Desc:		A simple visualizer to show the steps that eqn_solver executes to solve an equation.
+#			Reuses much of the code in eqn_vizualizer.py
+#
+# NOTE: 
+#		requires asp output in json format. As of gringo4 this is possible with
+#		clingo --outf=2 <gringo_file.lp>
+#		
+#		expects output from stdin, pipe clingo output to this program
+
+import sys
+import json
+import re
+from collections import defaultdict
+import pdb
+from eqn_vizualizer import *
+
+three_term	= re.compile("(\w+)\(([-\d\w]+),([-\d\w]+),([-\d\w]+)\)")
+
+def makeDefDictList():
+	return defaultdict(list)
+# forms an equation string for each step
+def formEqnStepString(predicates_list):
+	types, operator = defaultdict(dict), defaultdict(dict) 		# key is node id in all cases
+	mono , degree , coef = defaultdict(dict), defaultdict(dict), defaultdict(dict)
+	children = defaultdict(makeDefDictList)
+	all_steps = set()
+
+	for predicate in predicates_list:
+		matching = three_term.match(predicate)
+		if matching:
+			functor, arg1, arg2, step = matching.groups()
+			step = int(step)
+			all_steps.add(step)
+			if functor == 'type':
+				types[step][arg1] = arg2
+			elif functor == 'nodeOper':
+				operator[step][ arg1 ] = arg2
+			elif functor == 'parentOf':
+				children[step][ arg1 ].append( arg2 )
+			elif functor == 'nodeDeg':
+				degree[step][arg1 ]  = arg2
+			elif functor == 'nodeCoef':
+				coef[step][arg1] = arg2
+	every_step = []
+	print types
+	# for every step, lookup
+	for step in sorted(all_steps):
+		left	= formPolyString(types[step], operator[step],mono[step], degree[step], coef[step], children[step], '0')
+		right	= formPolyString(types[step], operator[step],mono[step], degree[step], coef[step], children[step], '1')
+		step_string =  left[1:-1] + '=' + right[1:-1]	# NOTE: slicing to avoid outermost parens
+		every_step.append(step_string)
+
+	return '\n'.join(every_step)
+
+def main():
+	clasp_output = ''.join(sys.stdin.xreadlines())
+	decoded = json.loads(clasp_output)
+	all_soln = decoded['Call'][0]['Witnesses']
+	for solution in all_soln:
+		print formEqnStepString(solution['Value'])
+
+if __name__ == "__main__":
+	main()
